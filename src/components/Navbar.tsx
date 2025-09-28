3c27ab1738001d32f7ca034a0a735653e99a1baa
luclucas9262 <lucaseleuterio95@gmail.com>
2025-09-28 16:06:07 -0300

    importe mudanças de cor na nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 import styles from './Navbar.module.css';
-import { Button } from '@mui/material';
+import { Button, } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,7 +15,25 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#5f230bff',
+      main: '#db3300ff',
+      dark: '#682312ff',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#0066ffff',
+      main: '#0044ffff',
+      dark: '#0022ffff',
+      contrastText: '#fff',
+    },
+  }
+});
+
   return (
+    <ThemeProvider theme={theme}>
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <Link href="/" className={styles.logo}>
@@ -33,22 +53,23 @@ const Navbar = () => {
           <span className={styles.menuIcon}></span>
         </button>
 
-        <div className={`${styles.menuItems} ${isMenuOpen ? styles.active : ''}`}>
-          <Button href="/" className={styles.navLink}>
+        <div className={`${styles.menuItems} ${isMenuOpen ? styles.active : ''}`} >
+          <Button href="/" className={styles.navLink} color="secondary">
             Home
           </Button>
-          <Button href="/cadastro" className={styles.navLink}>
+          <Button href="/cadastro" className={styles.navLink} color="secondary">
             Cadastro
           </Button>
-          <Button href="/agendar" className={styles.navLink}>
+          <Button href="/agendar" className={styles.navLink} color="secondary">
             Agendar
           </Button>
           <Button href="/login" variant="contained" color="primary">
-            Login
+            Login 
           </Button>
         </div>
       </div>
     </nav>
+    </ThemeProvider>
   );
 };
 
