eb370b619763cf5f8ef39ad5e67c5b9263359f9d
Vagner Carvalho <wagnerkarvalho54@gmail.com>
2025-11-04 13:54:31 -0300

    Update register.js

--- TCC- Sistema Psicosoft/js/register.js
+++ TCC- Sistema Psicosoft/js/register.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
             jsonData["Empresas"] = "PSICOSOFT";
             jsonData["id"] = jsonData["email"];
 
-            const url = 'https://6blopd43v4.execute-api.us-east-1.amazonaws.com/Alpha/cliente';
+            const url = 'https://6blopd43v4.execute-api.us-east-1.amazonaws.com/Alpha/paciente';
 
             fetch(url, {
                 method: 'PUT',
