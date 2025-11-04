edb1b160e04c793e78c8765032728175b9c80cbf
Vagner Carvalho <wagnerkarvalho54@gmail.com>
2025-11-04 10:20:24 -0300

    Update register.js

--- TCC- Sistema Psicosoft/js/register.js
+++ TCC- Sistema Psicosoft/js/register.js
@@ -66,7 +66,7 @@ if (loginForm) {
             return;
         }
 
-        const data = { id: email, password: password, empresa: "PSICOSOFT" };
+        const data = { email: email, password: password, empresa: "PSICOSOFT" };
         console.log("Enviando login:", data); // DEBUG
 
         try {
